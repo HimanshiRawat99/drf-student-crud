1020607d74b0adabd251d5bc0f95409639941022
Himanshirawat <himanshi04101999@gmail.com>
2026-02-21 15:02:41 +0530

    added git ignore

---
+++ crudproject/students/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Student
+from .serializers import StudentSerializer
+
+@api_view(['GET', 'POST'])
+def student_list(request):
+
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def student_detail(request, id):
+
+    try:
+        student = Student.objects.get(id=id)
+    except Student.DoesNotExist:
+        return Response({'error': 'Not found'}, status=404)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    if request.method == 'DELETE':
+        student.delete()
+        return Response(status=204)
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("DRF CRUD API is running")
